3fd4308cb7242ba00aa14ac15c9a80b293c629e0
MD. Mahmudul Hasan Pias <mhpias01@gmail.com>
2024-09-26 12:07:30 +0600

    Add files via upload

---
+++ 1247. Minimum Swaps to Make Strings Equal.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minimumSwap(string s1, string s2) {
+        int x = 0, y = 0, n = s1.length();
+        for (int i = 0; i < n; i++) {
+            if (s1[i]!= s2[i]) {
+                if (s1[i] != 'x') x++;
+                else y++;
+            }
+        }
+        if ((x + y) % 2 == 1) return -1;
+        return x / 2 + y / 2 + (x % 2) + (y%2);
+    }
+};
