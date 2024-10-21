6efc4a0943fa224c3cf41181befdede23da2927e
MD. Mahmudul Hasan Pias <mhpias01@gmail.com>
2024-10-21 22:06:48 +0600

    Update 867. Transpose Matrix.cpp

--- 867. Transpose Matrix.cpp
+++ 867. Transpose Matrix.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     vector<vector<int>> transpose(vector<vector<int>>& A) {
-        int ro=A.size();
-        int cl=A[0].size();
+        int ro = A.size();
+        int cl = A[0].size();
 
-        //creating a tow dimensional array and initializing it into 0
+        // creating a two dimensional array and initializing it into 0
 
-        vector<vector<int>>res(cl,vector<int>(ro,0));
+        vector<vector<int>> res(cl, vector<int>(ro));
 
-        for(int j=0;j<cl;j++){
-            for(int i=0;i<ro;i++){
-                res[j][i]=A[i][j];
+        for (int i = 0; i < ro; i++) {
+            for (int j = 0; j < cl; j++) {
+                res[j][i] = A[i][j];
             }
         }
         return res;
