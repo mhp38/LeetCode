38661adae5aaff3530ee234978299d693b541e24
MD. Mahmudul Hasan Pias <mhpias01@gmail.com>
2024-12-07 10:22:51 +0600

    Time: 0 ms (100.00%), Space: 12.6 MB (5.29%) - LeetHub

---
+++ 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.cpp
@@ -0,0 +1,36 @@
+/**
+Inorder Traversal
+
+The problem requires us to find the minimum difference between the values of any
+two nodes. Since the inorder traversal of a binary search tree is an increasing
+sequence, we only need to find the minimum difference between the values of two
+adjacent nodes in the inorder traversal.
+
+We can use a recursive method to implement the inorder traversal. During the
+process, we use a variable pre to save the value of the previous node. This way,
+we can calculate the minimum difference between the values of two adjacent nodes
+during the traversal.
+
+The time complexity is O ( n ) , and the space complexity is O ( n ) . Here, n
+is the number of nodes in the binary search tree.
+ */
+class Solution {
+public:
+    int ans = INT_MAX;
+    vector<int> tree;
+    void inord(TreeNode* root) {
+        if (root->left)
+            inord(root->left);
+        tree.push_back(root->val);
+        if (root->right)
+            inord(root->right);
+    }
+    int minDiffInBST(TreeNode* root) {
+        inord(root);
+        int sz = tree.size();
+        for (int i = 1; i < sz; i++) {
+            ans = min(ans, abs(tree[i] - tree[i - 1]));
+        }
+        return ans;
+    }
+};
