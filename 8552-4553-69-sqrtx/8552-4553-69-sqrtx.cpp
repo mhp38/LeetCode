5226db3a43930bc41d90dc5d5d32ce8bd2cfbcff
MD. Mahmudul Hasan Pias <mhpias01@gmail.com>
2025-01-07 17:18:14 +0600

    Time: 0 ms, Memory: 8.6 MB - LeetHub

--- 8552-4553-69-sqrtx/8552-4553-69-sqrtx.cpp
+++ 8552-4553-69-sqrtx/8552-4553-69-sqrtx.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        for (int i = 1;; i++) {
-            long long mul = (long long) i * i;
-            if (mul == x)
-                return i;
-            if (mul > x) {
-                return i - 1;
-            }
+        if (x == 0 || x == 1)
+            return x;
+
+        int left = 1, right = x / 2;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if ((long long)mid * mid > x)
+                right = mid - 1;
+            else
+                left = mid + 1;
         }
+        return right;
     }
 };
